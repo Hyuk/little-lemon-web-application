90eda5e9c9b3c4138bdca00d1f41105d475bbc53
Hyuk <hyukho83@gmail.com>
2024-07-05 10:49:24 +0900

    updated

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,11 +1,18 @@
 import Header from '../components/Header';
 import HeroPageTitle from '../components/HeroPageTitle';
+import Footer from '../components/Footer';
+import MenuCard from '../components/MenuCard';
+import { Box } from '@chakra-ui/react';
 
 export default function Menu() {
   return (
     <>
       <Header />
       <HeroPageTitle title="Menu" />
+      <Box maxW="980px" mx="auto" py="5em">
+        <MenuCard />
+      </Box>
+      <Footer />
     </>
   );
 }
